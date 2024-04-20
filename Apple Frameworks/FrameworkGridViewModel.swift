3466f21d3f8a05b6ba93abdb61808fa37b7ca882
Adam Khalifa <70577640+geekMe1982@users.noreply.github.com>
2024-04-20 17:46:42 +0300

    add a view model

---
+++ Apple Frameworks/FrameworkGridViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FrameworkDetailViewModel.swift
+//  Apple Frameworks
+//
+//  Created by Adam Khalifa on 20.04.2024.
+//
+
+import SwiftUI
+
+class FrameworkGridViewModel: ObservableObject {
+    
+    
+}
